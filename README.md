88bf313ebbb2a0c26cf883789852af10a7f9a7fc
GTXxXxXxX <mahe.gaillac@gmail.com>
2022-12-17 16:51:57 -0500

    AtomicTeller V.0.2.0
    Hi ! Finally hit the V.0.2.0
    
    - Added all the atoms
    - Totally new data base
    - Finished the first logic
    - First function of the program 100% usable
    
    - Language : French

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # AtomicTeller
 A Program that give you information about an atom.
+
+Actual version : V.0.1.3
+----------------------------------------------------------------------------------------------
+Salut tout le monde ! 
+
+Si vous lisez ça c'est que vous êtes intéréssés par mon premier programme. 
+
+Merci énormément !
+
+Si vous voulez me poser une question ou faire une remarque / critique : GTX#1339 sur discord !
+
+À bientôt !
+
+----------------------------------------------------------------------------------------------
+
+Hi everyone !
+
+If you are reading this it means that you are interested by my first project !
+
+Thanks a lot !
+
+If you want to ask me question or give me an advice / a review : GTX#1339 on discord !
+
+See you soon !
--- main.py
+++ main.py
@@ -1,63 +1,63 @@
 import sys  # import module sys
-
-
 class atom:  # create the class atom
-    def __init__(self,symbol,name,atomic_n,atomic_m,family,period,neutron,proton,electron, year,):
-        self.symbol = symbol
-        self.name = name
-        self.atomic_n = atomic_n
-        self.atomic_m = atomic_m
-        self.family = family
-        self.period = period
-        self.neutron = neutron
-        self.proton = proton
-        self.electron = electron
-        self.year = year
-
-    def __init__(self, splittedLine):
-        self.symbol = splittedLine[0]
+    def __init__(self, splittedLine):   #def the constructor of the class
+        self.atom_n = splittedLine[0]
         self.name = splittedLine[1]
-        self.atomic_n = splittedLine[2]
-        self.atomic_m = splittedLine[3]
-        self.family = splittedLine[4]
-        self.period = splittedLine[5]
-        self.neutron = splittedLine[6]
-        self.proton = splittedLine[7]
-        self.electron = splittedLine[8]
-        self.year = splittedLine[9]
+        self.symbol = splittedLine[2]
+        self.atom_m = splittedLine[3]
+        self.neutron = splittedLine[4]
+        self.proton = splittedLine[5]
+        self.electron = splittedLine[6]
+        self.period = splittedLine[7]
+        self.group = splittedLine[8]
+        self.phase = splittedLine[9]
+        self.radioactive = splittedLine[10]
+        self.natural = splittedLine[11]
+        self.type = splittedLine[12]
+        self.elecnegativity = splittedLine[13]
+        self.density = splittedLine[14]
+        self.discoverer = splittedLine[15]
+        self.year = splittedLine[16]
+        self.shells = splittedLine[17]
 
     def PrintMe(self):  # def PrintMe function
         print("")
         print(f"| Symbole chimique : {self.symbol}")
         print(f"| Nom : {self.name}")
-        print(f"| Numéro atomique : {self.atomic_n}")
-        print(f"| Masse atomique : {self.atomic_m}")
-        print(f"| Famille : {self.family}")
+        print(f"| Numéro atomique : {self.atom_n}")
+        print(f"| Masse atomique : {self.atom_m}")
+        print(f"| Famille : {self.type}")
+        print(f"| Groupe : {self.group}")
         print(f"| Période : {self.period}")
+        print(f"| Nombre de couches électroniques : {self.shells}")
         print(f"| Nombre de neutrons : {self.neutron}")
         print(f"| Nombre de protons : {self.proton}")
         print(f"| Nombre d'électrons : {self.electron}")
+        print(f"| Masse volumique (g/cm3) : {self.density}")
+        print(f"| Électronégativité : {self.elecnegativity}")
+        print(f"| Naturel ? : {self.natural}")
+        print(f"| Radioactif ? : {self.radioactive}")
+        print(f"| État : {self.phase}")
+        print(f"| Découvreur(s) : {self.discoverer}")
         print(f"| Année de découverte : {self.year}")
         print("")
 
 def __main__():  # def __main__ fonction
-    # Declare some atoms and put them in a array
+
     AtomsList  = []
-    CSVfile = open("AtomsDB.csv", "r")
-    Lines = CSVfile.readlines()
-    for line in Lines:
-        ValuesSplitted = line.rstrip(";\n").split(";")
-        if len(ValuesSplitted) == 10:
-            AtomsList.append(atom(ValuesSplitted))
-            # for value in ValuesSplitted:
-            #         print(value)
+    CSVfile = open("PTOFE.csv", "r")
+    CSVLines = CSVfile.readlines()
+    for line in CSVLines:
+        CSVLineSplitted = line.rstrip(";\n").split(";")
+        if len(CSVLineSplitted) == 18:
+            AtomsList.append(atom(CSVLineSplitted))
         else: 
             print("Incorrect line")
             print(line)
     WrongInputMessage = "Not found in database - try again"
     while True: 
 
-        InputString = str(input("Nom de l'atome / Numéro atomique / Symbol: ")).upper()
+        InputString = str(input("Nom de l'atome / Numéro atomique / Symbole : ")).upper()
         
         if InputString == "QUIT":
             sys.exit()
@@ -67,15 +67,11 @@ def __main__():  # def __main__ fonction
             if (
                 currentAtom.symbol.upper() == InputString 
                 or currentAtom.name.upper() == InputString
-                or currentAtom.atomic_n.upper() == InputString
+                or currentAtom.atom_n.upper() == InputString
             ):
                 currentAtom.PrintMe()
                 found = True
                 break
         if not found:
             print(WrongInputMessage)
-        
-        
-
-
 __main__()
